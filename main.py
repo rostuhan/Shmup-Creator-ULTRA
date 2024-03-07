fb65442b5d419601b5ebd4acd76b353dfa625f69
rostuhan <>
2024-03-07 17:16:34 +0200

    UI with tkinter

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import tkinter as tk
 import CreateFile
+Window = tk.Tk()
+Window.title("Shmup Creator ULTRA")
+Window.geometry("800x600")
+Title = tk.Label(text = "Shmup Creator ULTRA", font = ("", 25))
+Title.pack()
 CreateFile.CreateFile("game.bsc", "ULTRAgame.bsc")
+Window.mainloop()
